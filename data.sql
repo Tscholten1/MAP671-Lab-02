90ddd6d6b15b74cc05aae989a383151b0b8cbcb2
Tscholten1 <tsc244@uky.edu>
2019-04-22 16:42:22 -0500

    Initial commit

---
+++ data.sql
@@ -0,0 +1,41 @@
+/* Add your SQL below to extract analyze your state data from at least four layers */
+
+/* *************************************************************** */
+
+/* 1: Extract your state polygon */
+
+create table
+  my_table_name
+as
+select
+  *
+from
+  my_table_of_states
+
+"state" = "WI"
+
+
+/* *************************************************************** */
+
+/* 2: Extract your urban area polygons */
+
+"state" = "WI" or "state" = "WI_IL"
+
+/*extract towns*/
+"state" = 'WI'
+
+
+/* *************************************************************** */
+
+/* 3: Extract your waterbodies polygons */
+
+
+
+
+
+
+
+
+/* *************************************************************** */
+
+/* 4: Extract your stream lines ordered by size of stream */
